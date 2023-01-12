d9aaaa96a5bdd2d7e6e69fd8f96d4f6f708289d2
JonathanLCunnane <jonathan.cunnane@gmail.com>
2023-01-12 23:48:19 +0000

    Updated binary tree test to a benchmark.

--- app/binary_tree_test.rb
+++ app/binary_tree_test.rb
@@ -1,6 +1,23 @@
 require_relative 'binary_tree'
 require_relative 'grid'
+require_relative 'direction'
+require 'benchmark'
 
-grid = Grid.new(4, 4)
-BinaryTree.execute_on(grid)
-puts grid
+
+Benchmark.bm(20) do |benchmark|
+    grids = []
+    benchmark.report("Generating Grids"){(
+        grids = [
+            Grid.new(50, 50),
+            Grid.new(100, 100),
+            Grid.new(250, 250),
+            Grid.new(500, 500),
+            Grid.new(1000, 1000),
+            Grid.new(2500, 2500),
+        ]
+    )}
+    grids.each do |grid|
+        benchmark.report("%d x %d" % [grid.rows, grid.columns]) {BinaryTree.execute_on(grid)}
+    end
+end
+puts "Binary tree benchmark finished."
