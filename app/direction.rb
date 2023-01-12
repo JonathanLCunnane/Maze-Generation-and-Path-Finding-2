89a1ddf999af8f57edd0f82286de5b52e71768a0
JonathanLCunnane <jonathan.cunnane@gmail.com>
2023-01-12 19:39:45 +0000

    Added opposite? method.

--- app/direction.rb
+++ app/direction.rb
@@ -19,4 +19,8 @@ class Direction
     def self.west
         @@west
     end
+
+    def self.opposites?(dir_one, dir_two)
+        (dir_one == @@north && dir_two == @@south || dir_one == @@east && dir_two == @@west || dir_one == @@south && dir_two == @@north || dir_one == @@west && dir_two == @@east)
+    end
 end
