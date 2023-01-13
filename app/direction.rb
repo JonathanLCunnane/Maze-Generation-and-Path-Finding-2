8eccfb8121d1c67c01ef0f7937d06be1e415a58b
JonathanLCunnane <jonathan.cunnane@gmail.com>
2023-01-13 21:38:28 +0000

    Fixed formatting and made opposites method simpler

--- app/direction.rb
+++ app/direction.rb
@@ -1,26 +1,31 @@
+# frozen_string_literal: true
+require 'set'
+
+# A class to represent the four cardinal directions.
 class Direction
-    @@north = "NORTH"
-    @@east = "EAST"
-    @@south = "SOUTH"
-    @@west = "WEST"
+  def self.north
+    'NORTH'
+  end
 
-    def self.north
-        @@north
-    end
+  def self.east
+    'EAST'
+  end
 
-    def self.east
-        @@east
-    end
+  def self.south
+    'SOUTH'
+  end
 
-    def self.south
-        @@south
-    end
+  def self.west
+    'WEST'
+  end
 
-    def self.west
-        @@west
-    end
-
-    def self.opposites?(dir_one, dir_two)
-        (dir_one == @@north && dir_two == @@south || dir_one == @@east && dir_two == @@west || dir_one == @@south && dir_two == @@north || dir_one == @@west && dir_two == @@east)
-    end
+  def self.opposites?(dir_one, dir_two)
+    conditions = [
+      [north, south],
+      [south, north],
+      [east, west],
+      [west, east]
+    ]
+    conditions.include?([dir_one, dir_two])
+  end
 end
