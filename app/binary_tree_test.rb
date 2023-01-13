89060b94d453e0fba4ca04095306daa909f37203
JonathanLCunnane <jonathan.cunnane@gmail.com>
2023-01-13 09:12:40 +0000

    Fixed formatting.

--- app/binary_tree_test.rb
+++ app/binary_tree_test.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require_relative 'binary_tree'
 require_relative 'grid'
 require_relative 'direction'
@@ -5,19 +7,20 @@ require 'benchmark'
 
 
 Benchmark.bm(20) do |benchmark|
-    grids = []
-    benchmark.report("Generating Grids"){(
-        grids = [
-            Grid.new(50, 50),
-            Grid.new(100, 100),
-            Grid.new(250, 250),
-            Grid.new(500, 500),
-            Grid.new(1000, 1000),
-            Grid.new(2500, 2500),
-        ]
-    )}
-    grids.each do |grid|
-        benchmark.report("%d x %d" % [grid.rows, grid.columns]) {BinaryTree.execute_on(grid)}
-    end
+  grids = []
+  benchmark.report('Generating Grids') do
+    grids = [
+      Grid.new(50, 50),
+      Grid.new(100, 100),
+      Grid.new(250, 250),
+      Grid.new(500, 500),
+      Grid.new(1000, 1000),
+      Grid.new(2500, 2500)
+    ]
+  end
+  grids.each do |grid|
+    benchmark.report('%<grid.rows>d x %<grid.columns>d') { BinaryTree.execute_on(grid) }
+  end
 end
-puts "Binary tree benchmark finished."
+
+puts 'Binary tree benchmark finished.'
