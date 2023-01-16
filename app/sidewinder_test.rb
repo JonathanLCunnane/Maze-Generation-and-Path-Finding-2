308679be3330f5eb3135bb349d594fe7a2456e6c
JonathanLCunnane <jonathan.cunnane@gmail.com>
2023-01-16 19:52:30 +0000

    Created a test file for the sidewinder algorithm.

---
+++ app/sidewinder_test.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+require_relative 'sidewinder'
+require_relative 'grid'
+require_relative 'direction'
+require 'benchmark'
+
+
+out = +''
+Benchmark.bm(20) do |benchmark|
+  grids = []
+  benchmark.report('Generating Grids') do
+    grids = [
+      Grid.new(5, 5),
+      Grid.new(10, 10),
+      Grid.new(25, 25),
+      Grid.new(50, 50),
+      Grid.new(100, 100),
+      Grid.new(250, 250),
+      Grid.new(500, 500),
+      Grid.new(1000, 1000),
+      Grid.new(2500, 2500)
+    ]
+  end
+  grids.each do |grid|
+    benchmark.report("#{grid.rows} x #{grid.columns}") { Sidewinder.execute_on(grid) }
+    out << "#{grid}\n\n" if grid.columns <= 25
+  end
+end
+
+puts 'Sidewinder benchmark finished.'
+puts 'Grids for columns <= 25 are below:'
+puts out
