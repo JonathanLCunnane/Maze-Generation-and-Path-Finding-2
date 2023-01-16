15a5a753c5512cb2f6ee88156f830f2e0910e2c4
JonathanLCunnane <jonathan.cunnane@gmail.com>
2023-01-16 19:13:49 +0000

    Updated benchmark process.

--- app/binary_tree_test.rb
+++ app/binary_tree_test.rb
@@ -6,10 +6,14 @@ require_relative 'direction'
 require 'benchmark'
 
 
+out = +''
 Benchmark.bm(20) do |benchmark|
   grids = []
   benchmark.report('Generating Grids') do
     grids = [
+      Grid.new(5, 5),
+      Grid.new(10, 10),
+      Grid.new(25, 25),
       Grid.new(50, 50),
       Grid.new(100, 100),
       Grid.new(250, 250),
@@ -20,7 +24,10 @@ Benchmark.bm(20) do |benchmark|
   end
   grids.each do |grid|
     benchmark.report("#{grid.rows} x #{grid.columns}") { BinaryTree.execute_on(grid) }
+    out << "#{grid}\n\n" if grid.columns <= 25
   end
 end
 
 puts 'Binary tree benchmark finished.'
+puts 'Grids for columns <= 25 are below:'
+puts out
