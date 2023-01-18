fe99139fde836fed78043b827f0ff6bd47bf8828
JonathanLCunnane <jonathan.cunnane@gmail.com>
2023-01-18 17:21:31 +0000

    Updated benchmark process.

--- app/sidewinder_test.rb
+++ app/sidewinder_test.rb
@@ -7,6 +7,7 @@ require 'benchmark'
 
 
 out = +''
+time = Time.new
 Benchmark.bm(20) do |benchmark|
   grids = []
   benchmark.report('Generating Grids') do
@@ -17,17 +18,18 @@ Benchmark.bm(20) do |benchmark|
       Grid.new(50, 50),
       Grid.new(100, 100),
       Grid.new(250, 250),
-      Grid.new(500, 500),
-      Grid.new(1000, 1000),
-      Grid.new(2500, 2500)
+      Grid.new(500, 500)
     ]
   end
   grids.each do |grid|
     benchmark.report("#{grid.rows} x #{grid.columns}") { Sidewinder.execute_on(grid) }
     out << "#{grid}\n\n" if grid.columns <= 25
+    curr_time = time.strftime('%Y_%m_%d_%H_%M_%S')
+    grid.to_png.save "./out/sidewinder_#{curr_time}_#{grid.rows}_#{grid.columns}.png"
   end
 end
 
 puts 'Sidewinder benchmark finished.'
+puts "Grid pngs have been stored in the 'out' folder"
 puts 'Grids for columns <= 25 are below:'
 puts out
