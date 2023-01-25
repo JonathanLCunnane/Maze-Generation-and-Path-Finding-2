f7d121dd61714f8af83b9efb51c3aed120dca9ef
JonathanLCunnane <jonathan.cunnane@gmail.com>
2023-01-25 23:02:09 +0000

    Added a way to test the new ColourGrid class.

---
+++ app/colouring_test.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require_relative 'colour_grid'
+require_relative 'sidewinder'
+require_relative 'binary_tree'
+
+def get_and_save_png(generation_alg, alg_name)
+  grid = ColourGrid.new(75, 75)
+  generation_alg.call(grid)
+  start = grid[grid.rows / 2, grid.columns / 2]
+  grid.distances = start.distances
+
+  time = Time.new
+  curr_time = time.strftime('%Y_%m_%d_%H_%M_%S')
+  grid.to_png.save "./out/#{alg_name}_colouring_test_#{curr_time}_#{grid.rows}_#{grid.columns}.png"
+end
+
+[
+  [Sidewinder.method(:execute_on), 'sidewinder'],
+  [BinaryTree.method(:execute_on), 'binary_tree']
+].each do |generation_alg, alg_name|
+  get_and_save_png(generation_alg, alg_name)
+end
+
+puts "Grid pngs have been stored in the 'out' folder"
