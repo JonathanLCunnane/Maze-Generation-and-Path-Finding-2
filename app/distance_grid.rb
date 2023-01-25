07530a25fcafcf5372c0484e88778122cd26b7cf
JonathanLCunnane <jonathan.cunnane@gmail.com>
2023-01-25 22:20:18 +0000

    Fixed formatting

--- app/distance_grid.rb
+++ app/distance_grid.rb
@@ -8,8 +8,8 @@ class DistanceGrid < Grid
   attr_accessor :distances
 
   def contents_of(cell)
-    if distances && distances[cell]
-      distances[cell].to_s(36)
+    if @distances && @distances[cell]
+      @distances[cell].to_s(36)
     else
       super(cell)
     end
