36e80f778b54ea2e156b4910b055d5bbfed8181a
JonathanLCunnane <jonathan.cunnane@gmail.com>
2023-01-25 20:31:57 +0000

    Updated Dijkstra test to show the application of the new max function in distances.rb by finding the longest path in a grid.

--- app/dijkstra_test.rb
+++ app/dijkstra_test.rb
@@ -4,13 +4,22 @@ require_relative 'distance_grid'
 require_relative 'sidewinder'
 
 
-grid = DistanceGrid.new(8, 8)
-Sidewinder.execute_on(grid, 0.75)
+grid = DistanceGrid.new(7, 7)
+Sidewinder.execute_on(grid)
+
+puts 'Empty grid:'
+puts grid
 
 root = grid[0, 0]
 distances = root.distances
 grid.distances = distances
+puts "\n\nDistances from root = [0, 0]:"
 puts grid
 
-grid.distances = distances.path_to(grid[7, 0])
+# To find the longest path in a maze run dijkstra twice like shown.
+start_cell = distances.max[0]
+distances_from_start = start_cell.distances
+end_cell, max_grid_dist = distances_from_start.max
+grid.distances = distances_from_start.path_to(end_cell)
+puts "\n\nLongest path in grid (with a distance of #{max_grid_dist}): "
 puts grid
