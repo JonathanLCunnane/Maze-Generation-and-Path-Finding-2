281274ae9bcf831b8c3734788a501d9e6dea0dd8
JonathanLCunnane <jonathan.cunnane@gmail.com>
2023-01-26 21:20:46 +0000

    Added the Aldous-Broder unbiased generation algorithm.

---
+++ app/aldous_broder.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+# A class which implements the Aldous-Broder unbiased, random generation algorithm.
+class AldousBroder
+  def self.execute_on(grid)
+    curr_cell = grid.random_cell
+    to_visit = grid.count - 1
+    while to_visit.positive?
+      puts "#{curr_cell.row}, #{curr_cell.column}" if curr_cell.neighbours.length == 0
+      next_cell = curr_cell.neighbours.sample
+
+      to_visit -= 1 if link_if_unvisited(curr_cell, next_cell)
+
+      curr_cell = next_cell
+    end
+
+    grid
+  end
+
+  def self.link_if_unvisited(curr_cell, next_cell)
+    if next_cell.links.empty?
+      curr_cell.link(next_cell)
+      true
+    else
+      false
+    end
+  end
+
+  private_class_method :link_if_unvisited
+
+end
---
+++ app/aldous_broder_test.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+require_relative 'aldous_broder'
+require_relative 'grid'
+require_relative 'direction'
+require 'benchmark'
+
+
+out = +''
+time = Time.new
+Benchmark.bm(20) do |benchmark|
+  grids = []
+  benchmark.report('Generating Grids') do
+    grids = [
+      Grid.new(5, 5),
+      Grid.new(10, 10),
+      Grid.new(25, 25),
+      Grid.new(50, 50),
+      Grid.new(100, 100),
+      Grid.new(250, 250),
+      Grid.new(500, 500)
+    ]
+  end
+  grids.each do |grid|
+    benchmark.report("#{grid.rows} x #{grid.columns}") { AldousBroder.execute_on(grid) }
+    out << "#{grid}\n\n" if grid.columns <= 25
+    curr_time = time.strftime('%Y_%m_%d_%H_%M_%S')
+    grid.to_png.save "./out/aldous_broder_#{curr_time}_#{grid.rows}_#{grid.columns}.png"
+  end
+end
+
+puts 'Aldous-Broder benchmark finished.'
+puts "Grid pngs have been stored in the 'out' folder"
+puts 'Grids for columns <= 25 are below:'
+puts out
