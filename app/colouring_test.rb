7aa529629b28a6fdf1e597bd1d1db07fae47fa15
JonathanLCunnane <jonathan.cunnane@gmail.com>
2023-01-26 21:22:25 +0000

    Added the Aldous-Broder generation algorithm to the coloring test.

--- app/colouring_test.rb
+++ app/colouring_test.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 require_relative 'colour_grid'
-require_relative 'sidewinder'
 require_relative 'binary_tree'
+require_relative 'sidewinder'
+require_relative 'aldous_broder'
 
 def get_and_save_png(generation_alg, alg_name)
   grid = ColourGrid.new(75, 75)
@@ -17,7 +18,8 @@ end
 
 [
   [Sidewinder.method(:execute_on), 'sidewinder'],
-  [BinaryTree.method(:execute_on), 'binary_tree']
+  [BinaryTree.method(:execute_on), 'binary_tree'],
+  [AldousBroder.method(:execute_on), 'aldous_broder']
 ].each do |generation_alg, alg_name|
   get_and_save_png(generation_alg, alg_name)
 end
