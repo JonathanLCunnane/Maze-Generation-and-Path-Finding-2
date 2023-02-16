3087d62ece5b6f346822628a642f733be1f7cd6f
JonathanLCunnane <jonathan.cunnane@gmail.com>
2023-02-16 20:48:51 +0000

    Updated image mask test.

--- app/image_mask_test.rb
+++ app/image_mask_test.rb
@@ -3,6 +3,7 @@
 require_relative 'mask'
 require_relative 'masked_grid'
 require_relative 'recursive_backtracker'
+require_relative 'distances'
 
 abort 'Image mask .png file must be provided in args' if ARGV.empty?
 
@@ -12,7 +13,16 @@ RecursiveBacktracker.execute_on(grid)
 
 time = Time.new
 curr_time = time.strftime('%Y_%m_%d_%H_%M_%S')
-out = "./out/recursive_backtracker_ascii_mask_test_#{curr_time}_#{grid.rows}_#{grid.columns}.png"
+out = "./out/recursive_backtracker_image_mask_test_#{curr_time}_#{grid.rows}_#{grid.columns}.png"
 
 grid.to_png.save(out)
-puts "Saved to 'out' folder"
+puts "Uncoloured grid saved to 'out' folder"
+
+start = grid.random_cell
+distances = start.distances
+grid.distances = distances
+
+curr_time = time.strftime('%Y_%m_%d_%H_%M_%S')
+out = "./out/recursive_backtracker_image_mask_coloured_test_#{curr_time}_#{grid.rows}_#{grid.columns}.png"
+grid.to_png.save(out)
+puts "Coloured grid saved to 'out' folder"
